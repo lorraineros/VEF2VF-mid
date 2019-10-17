464ff0f67b9ede42de68d733d7a7bfd7c49d0db1
Lorraine Rós <lorrainert2002@gmail.com>
2019-10-17 19:07:34 +0000

    ok

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, flash, redirect, render_template, request, json
 import os
 import urllib.request
+from math import inf
 from jinja2 import ext
 from datetime import datetime
 
@@ -11,20 +12,25 @@ with urllib.request.urlopen("https://apis.is/petrol") as url:
     gogn =json.loads(url.read().decode())
 
 def format_time(gogn):
-    return datetime.strptime(data, '%Y-%m-%d%H:%M:%S').strftime('%d. %B. %Y. Tími %H:%M')
+    return datetime.strptime(gogn, '%Y-%m-%dT%H:%M:%S.%f').strftime('%d/%m/%Y Klukkan %H:%M')
 
 app.jinja_env.add_extension(ext.do)
 app.jinja_env.filters['format_time'] = format_time
 
-def minPetrol():
-    pass
+def find_lowest_prices(gogn):
+    lowest_petrol_price = [min(gogn['results'], key=lambda x: x['bensin95']), min(gogn['results'], key=lambda x: x[
+        'bensin95_discount'] if x['bensin95_discount'] is not None else inf)]
+    lowest_diesel_price = [min(gogn['results'], key=lambda x: x['diesel']), min(gogn['results'],
+                           key=lambda x: x['diesel_discount'] if x['diesel_discount'] is not None else inf)]
 
-def minDiesel():
-    pass
+    lowest_petrol_price = lowest_petrol_price[0] if lowest_petrol_price[0]['bensin95'] <= lowest_petrol_price[1]['bensin95_discount'] else lowest_petrol_price[1]
+    lowest_diesel_price = lowest_diesel_price[0] if lowest_diesel_price[0]['diesel'] <= lowest_diesel_price[1]['diesel_discount'] else lowest_diesel_price[1]
+
+    return lowest_petrol_price, lowest_diesel_price
 
 @app.route('/')
 def index():
-    return render_template('index.tpl', gogn=gogn, minP=minPetrol(),minD=minDiesel())
+    return render_template('index.tpl', gogn=gogn, lowestPrices=find_lowest_prices(gogn))
     
 @app.route('/company/<company>')
 def comp(company):
--- templates/index.tpl
+++ templates/index.tpl
@@ -15,4 +15,24 @@
         {% endif %}
     {% endfor %}
 </div>
+<div class="kort">
+	<h2>Besta verðið</h2>
+    {% if lowestPrices.0.bensin95_discount == None %}
+        <h4>Bensín: {{ lowestPrices.0.bensin95 }} krónur hjá {{ lowestPrices.0.company }}</h4>
+    {% elif lowestPrices.0.bensin95 <= lowestPrices.0.bensin95_discount %}
+        <h4>Bensín: {{ lowestPrices.0.bensin95 }} krónur hjá {{ lowestPrices.0.company }}</h4>
+    {% else %}
+        <h4>Bensín: {{ lowestPrices.0.bensin95_discount }} krónur hjá {{ lowestPrices.0.company }}</h4>
+    {% endif %}
+    {% if lowestPrices.1.diesel_discount == None %}
+        <h4>Díesel: {{ lowestPrices.1.diesel }} krónur hjá {{ lowestPrices.1.company }}</h4>
+    {% elif lowestPrices.1.diesel <=  lowestPrices.1.diesel_discount %}
+        <h4>Díesel: {{ lowestPrices.1.diesel }} krónur hjá {{ lowestPrices.1.company }}</h4>
+    {% else %}
+        <h4>Díesel: {{ lowestPrices.1.diesel_discount }} krónur hjá {{ lowestPrices.1.company }}</h4>
+    {% endif %}
+</div>
+<div>
+    <p>Síðast uppfært: {{ gogn.timestampPriceCheck|format_time }}</p>
+</div>
 {% endblock %}
